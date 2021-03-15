d356cdc085ecea6d432f6640aa3b5bc9b899374e
Alex Setia <alextheprogrammer21@gmail.com>
2021-03-15 18:26:02 -0400

    Add comments to scripts

--- scripts/api.js
+++ scripts/api.js
@@ -1,8 +1,10 @@
-$(document).ready(() => {
-  
-  $.get( "https://cors-anywhere.herokuapp.com/https://loripsum.net/api", function( data ) {
-    $( ".api-call" )
-    .append( data )
-  });
+//Used for API calls
 
-})
+$(document).ready(() => {
+  $.get(
+    "https://cors-anywhere.herokuapp.com/https://loripsum.net/api",
+    function (data) {
+      $(".api-call").append(data);
+    }
+  );
+});
